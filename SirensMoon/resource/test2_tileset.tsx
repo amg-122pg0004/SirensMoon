d5fa6fdb657108e501a6f313925cf30a05515319
122pg0004@tokyo.amg.ac.jp <122pg0004@tokyo.amg.ac.jp>
2022-07-04 18:36:36 +0900

    [update]テスト用マップを更新

---
+++ SirensMoon/resource/test2_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tile_test" tilewidth="30" tileheight="30" tilecount="40" columns="8">
+ <image source="tile_test.png" width="240" height="150"/>
+</tileset>
