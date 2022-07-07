95a08920d415c5eef791e0fa33b46bdedd4da810
122pg0004@tokyo.amg.ac.jp <122pg0004@tokyo.amg.ac.jp>
2022-07-07 17:36:47 +0900

    [update]ChangeCamera関数の追加

--- SirensMoon/Camera.cpp
+++ SirensMoon/Camera.cpp
@@ -5,32 +5,63 @@
 Camera::Camera(Game& game,SplitWindow& sw) :
 	_game{ game },
 	_splitWindow{sw},
-	_cameraPosition{0,0},
-	_speed{10}
+	_pos{0,0},
+	_speed{10},
+	_stageSize{3,3}
  {
+
 }
+
+void Camera::ChangePosition(Camera::ChangeDir dir) {
+	switch (dir) {
+
+		case Camera::ChangeDir::UP :
+			_pos.y = _pos.y - screen_H;
+			break;
+		case Camera::ChangeDir::DOWN:
+			_pos.y = _pos.y - screen_H;
+			break;
+		case Camera::ChangeDir::LEFT:
+			_pos.x = _pos.x - screen_W/2;
+			break;
+		case Camera::ChangeDir::RIGHT:
+			_pos.x = _pos.x + screen_W/2;
+			break;
+	}
+	if (_pos.x < 0) {
+		_pos.x = 0;
+	}
+	if (_pos.x > screen_W/2*_stageSize.x) {
+		_pos.x = screen_W / 2 * _stageSize.x;
+	}
+	if (_pos.y < 0) {
+		_pos.y = 0;
+	}
+	if (_pos.x > screen_H * _stageSize.y) {
+		_pos.x = screen_H * _stageSize.y;
+	}
+}
+
 
 void Camera::Update(int playernum) {
 	_inputManager = _game.GetInputManager();
 	if(_inputManager->CheckInput("UP", 'h',playernum)) {
-		_cameraPosition.y = _cameraPosition.y + _speed;
+		_pos.y = _pos.y + _speed;
 	}
 	if (_inputManager->CheckInput("DOWN", 'h',playernum)) {
-		_cameraPosition.y = _cameraPosition.y - _speed;
+		_pos.y = _pos.y - _speed;
 	}
 	if (_inputManager->CheckInput("LEFT", 'h', playernum)) {
-		_cameraPosition.x = _cameraPosition.x + _speed;
+		_pos.x = _pos.x + _speed;
 	}
 	if (_inputManager->CheckInput("RIGHT", 'h', playernum)) {
-		_cameraPosition.x = _cameraPosition.x - _speed;
+		_pos.x = _pos.x - _speed;
 	}
 }
 
 void Camera::Render(int x,int y) {
 	std::stringstream ss;
-	ss <<"カメラX座標"<< _cameraPosition.x << "\n";
-	ss << "カメラY座標" << _cameraPosition.y << "\n";
+	ss <<"カメラX座標"<< _pos.x << "\n";
+	ss << "カメラY座標" << _pos.y << "\n";
 	DrawString(x, y, ss.str().c_str(), GetColor(255,255,255));
 }
-
-
