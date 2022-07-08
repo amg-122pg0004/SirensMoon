b678c8850690a184914863b846c86dac90fdf24a
122pg0004@tokyo.amg.ac.jp <122pg0004@tokyo.amg.ac.jp>
2022-07-08 13:42:02 +0900

    [update]円周率計算、max,min,clampを追加

--- SirensMoon/Math.h
+++ SirensMoon/Math.h
@@ -70,3 +70,34 @@ public:
         return temp;
     }
 };
+
+namespace Math {
+    // 円周率
+    constexpr float Pi = 3.1415926535f;
+    constexpr float TwoPi = Pi * 2.0f;
+    constexpr float PiOver2 = Pi / 2.0f;
+
+    // 度数から孤度(ラジアン)に変換
+    inline float ToRadians(float degrees) {
+        return degrees * Pi / 180.0f;
+    }
+    // 孤度から度数(ディグリー)に変換
+    inline float ToDegrees(float radians) {
+        return radians * 180.0f / Pi;
+    }
+    // 二つの値の大きい値を取得
+    template <typename T>
+    T Max(const T& a, const T& b) {
+        return (a < b ? b : a);
+    }
+    // 二つの値の小さい値を取得
+    template <typename T>
+    T Min(const T& a, const T& b) {
+        return (a < b ? a : b);
+    }
+    // 二つの値の間でクランプする
+    template <typename T>
+    T Clamp(const T& value, const T& lower, const T& upper) {
+        return Min(upper, Max(lower, value));
+    }
+}
