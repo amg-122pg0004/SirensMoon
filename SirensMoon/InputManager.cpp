d8458ec65eaab32e991def9f9f4d9398c3a32b2d
122pg0004@tokyo.amg.ac.jp <122pg0004@tokyo.amg.ac.jp>
2022-07-08 13:43:57 +0900

    [update]アナログスティックの入力を読み込むように変更

--- SirensMoon/InputManager.cpp
+++ SirensMoon/InputManager.cpp
@@ -12,24 +12,29 @@
 #include <sstream>
 
 InputManager::InputManager() {
-	KeyState= {
+	_keyState= {
 			{"UP",PAD_INPUT_UP,false,false,0},
 			{"DOWN",PAD_INPUT_DOWN,false,false,0},
 			{"LEFT",PAD_INPUT_LEFT,false,false,0},
 			{"RIGHT",PAD_INPUT_RIGHT,false,false,0},
-			{"ACTION",PAD_INPUT_10,false,false,0},
+			{"ACTION",PAD_INPUT_1,false,false,0},
 			{"UP",PAD_INPUT_UP,false,false,1},
 			{"DOWN",PAD_INPUT_DOWN,false,false,1},
 			{"LEFT",PAD_INPUT_LEFT,false,false,1},
 			{"RIGHT",PAD_INPUT_RIGHT,false,false,1},
 			{"ACTION",PAD_INPUT_1,false,false,1},
 	};
+
+	_analogState = {
+		{{0,0},0},
+		{{0,0},1}
+	};
 }
 
 /**@brief 使用する各キーについてインプット状態を確認して保存 */
 void InputManager::InputUpdate() {
 
-	for (auto&& key : KeyState) {
+	for (auto&& key :_keyState) {
 		switch (key.PadNo) {
 		case 0:
 			if (GetJoypadInputState(DX_INPUT_KEY_PAD1) & key.KeyName) {
@@ -62,6 +67,22 @@ void InputManager::InputUpdate() {
 		key.Trigger = false;
 		key.Hold = false;
 	}
+
+	for (auto&& analog : _analogState) {
+		int InputX, InputY;
+		switch (analog.PadNo) {
+		case 0:
+			GetJoypadAnalogInput(&InputX, &InputY, DX_INPUT_KEY_PAD1);
+			analog.Value = { static_cast<double>(InputX),static_cast<double>(InputY) };
+			continue;
+		case 1:
+			GetJoypadAnalogInput(&InputX, &InputY, DX_INPUT_PAD2);
+			analog.Value = { static_cast<double>(InputX),static_cast<double>(InputY) };
+			continue;
+		default:
+			break;
+		}
+	}
 }
 
 /**
@@ -72,10 +93,10 @@ void InputManager::InputUpdate() {
  * \return 入力があればtrue
  */
 bool InputManager::CheckInput(const std::string actionname, const char keystate,int playernum) {
-	for (auto&& key : KeyState)
-		if (key.ActionName == actionname && key.PadNo==playernum) 
+	for (auto&& key : _keyState) {
+		if (key.ActionName == actionname && key.PadNo == playernum)
 		{
-			switch (keystate) 
+			switch (keystate)
 			{
 			case 'h':
 				return key.Hold;
@@ -88,13 +109,28 @@ bool InputManager::CheckInput(const std::string actionname, const char keystate,
 				break;
 			}
 		}
+
+	}
 	return false;
+}
+
+Vector2 InputManager::CheckAnalogInput(const int playernum) {
+	for (auto&& analog : _analogState) {
+		if (analog.PadNo == playernum) {
+			return analog.Value;
+			break;
+		}
+	}
+	return { 0,0 };
 }
 
 void InputManager::Render() {
 	std::stringstream ss;
-	for (auto&& key : KeyState) {
+	for (auto&& key : _keyState) {
 		ss << key.ActionName <<" "<< key.KeyName <<" " << key.Hold <<" "<< key.Trigger << "\n";
+	}
+	for (auto&& analog : _analogState) {
+		ss  <<"アナログスティックプレイヤー"<<analog.PadNo<<" "<< analog.Value.x <<" "<<analog.Value.y << "\n";
 	}
 
 	DrawString(50, 100, ss.str().c_str(), GetColor(255, 255, 255));
--- SirensMoon/InputManager.h
+++ SirensMoon/InputManager.h
@@ -6,6 +6,7 @@
  * \date   June 2022
  *********************************************************************/
 #pragma once
+#include "Math.h"
 #include <vector>
 #include <string>
 
@@ -13,6 +14,8 @@ class InputManager{
 
 	public:
 		
+		
+
 		InputManager();//<コンストラクタ
 
 		/**@brief 使用する各キーについてインプット状態を確認して保存 */
@@ -26,6 +29,15 @@ class InputManager{
 		* \return 入力があればtrue
 		*/
 		bool CheckInput(const std::string actionname,const char keystate,int playernum);
+
+		/**
+		* @brief アナログスティックの入力状態を確認する
+		*
+		* \param playernum 何個目のコントローラーか
+		* \return 入力状態をそのまま返す
+		*/
+		Vector2 CheckAnalogInput(const int playernum);
+
 #ifdef _DEBUG
 		void Render();
 #endif 
@@ -42,6 +54,15 @@ private:
 		int PadNo;				//<入力したコントローラーの番号
 	};
 
+	struct AnalogInfo {
+		Vector2 Value;
+		int PadNo;
+	};
+
+
 	/*ゲーム内で使用する各アクションについての入力状態を保存する*/
-	std::vector<KeyInfo> KeyState;
+	std::vector<KeyInfo> _keyState;
+	
+	/*アナログスティックの入力情報*/
+	std::vector<AnalogInfo> _analogState;
 };
