cbe24fe71fa243b2c01378d94f09011d478a8fc5
122pg0004@tokyo.amg.ac.jp <122pg0004@tokyo.amg.ac.jp>
2022-07-21 17:02:45 +0900

    [update]テストマップ更新

--- SirensMoon/resource/test.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.0" name="test" tilewidth="30" tileheight="30" tilecount="9" columns="3">
- <image source="../../../../../../Downloads/iOS の画像.jpg" width="90" height="90"/>
-</tileset>
