712103a0b440f086c4855f2f36d65712e54f1883
122pg0004@tokyo.amg.ac.jp <122pg0004@tokyo.amg.ac.jp>
2022-07-27 17:45:43 +0900

    [update]出現時にサーバーで抽選済みの見た目になるようにした。また、サーバーがアクセスされていなければ消えるようにした。

--- SirensMoon/EnemyVIP.cpp
+++ SirensMoon/EnemyVIP.cpp
@@ -1,16 +1,19 @@
 #include "EnemyVIP.h"
-#include "Math.h"
-#include "ImageServer.h"
-#include "ModeGame.h"
-#include "BlinkLight.h"
-#include "SoundServer.h"
-#include <random>
+#include "ServerMachine.h"
 
 EnemyVIP::EnemyVIP(Game& game, ModeBase& mode, MapChips::EnemyData enemydata, ServerMachine& owner) :Enemy(game, mode, enemydata), _owner{ owner }
 {
 	_generatedEnemy=_owner.GetGeneratedEnemy();
+	Init();
 }
 
 void EnemyVIP::Init() {
 	Enemy::Init();
+}
+
+void EnemyVIP::Update() {
+	Enemy::Update();
+	if (!_owner.GetValidFlag()) {
+		_dead = true;
+	}
 }
--- SirensMoon/EnemyVIP.h
+++ SirensMoon/EnemyVIP.h
@@ -1,11 +1,13 @@
 #pragma once
 #include "Enemy.h"
-#include "ServerMachine.h"
+
+class ServerMachine;
 
 class EnemyVIP : public Enemy {
 public:
 	EnemyVIP(Game& game, ModeBase& mode, MapChips::EnemyData enemydata,ServerMachine& owner);
 	void Init() override;
+	void Update() override;
 private:
 	ServerMachine& _owner;
 };
