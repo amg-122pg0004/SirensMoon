387c6a700b3a3844308d386d0d97b218c435065e
122pg0004@tokyo.amg.ac.jp <122pg0004@tokyo.amg.ac.jp>
2022-07-28 16:56:57 +0900

    [update]テスト用マップ（追加画像1枚に合体した版）

---
+++ SirensMoon/resource/Tileset2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="iOS の画像" tilewidth="30" tileheight="30" tilecount="55" columns="11">
+ <image source="C:/Users/amg/Downloads/iOS の画像.jpg" width="354" height="177"/>
+</tileset>
