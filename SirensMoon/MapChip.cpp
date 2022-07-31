f96a1d00b80f3863f7e4802153d14ba8353593ee
122pg0004@tokyo.amg.ac.jp <122pg0004@tokyo.amg.ac.jp>
2022-08-01 06:46:26 +0900

    [update]テスト素材整理

--- SirensMoon/MapChip.cpp
+++ SirensMoon/MapChip.cpp
@@ -10,7 +10,7 @@
 MapChips::MapChips(Game& game, ModeBase& mode) :_game{ game }, _mode{mode}{
 	_mapTileData.clear();
 	_minimapData.clear();
-	LoadMap("resource/", "TestMap_02.json");
+	LoadMap("resource/", "TestMap_01.json");
 }
 
 MapChips::~MapChips() {
