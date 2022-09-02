5c8ba4a3cf117706b03def71116d5eedb6d682c3
122pg0004@tokyo.amg.ac.jp <122pg0004@tokyo.amg.ac.jp>
2022-09-02 19:46:13 +0900

    [update]スタート画面更新

--- SirensMoon/Game.cpp
+++ SirensMoon/Game.cpp
@@ -19,7 +19,7 @@ Game::Game() :_frameCount{0},_progress{Progress::StartMenu}
 	_modeServer = std::make_unique<ModeServer>(*this);
 	_inputManager = std::make_unique<InputManager>();
 
-	PlayStage2();
+	PlayStartMenu();
 }
 
 void Game::Input() {
